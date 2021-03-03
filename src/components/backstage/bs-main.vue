96d3ae12f022355cd22b3364dca36ba5c16663b7
Promise-jun <1022988217@qq.com>
2021-03-03 21:46:25 +0800

    后台管理系统：菜单、头部、会员管理

--- src/components/backstage/bs-main.vue
+++ src/components/backstage/bs-main.vue
@@ -1,9 +1,55 @@
 <template>
   <div>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside>
+        <el-menu
+          default-active="/backstage/member"
+          class=""
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#304156"
+          text-color="#fff"
+          active-text-color="#1890ff"
+          :router="true"
+        >
+          <el-menu-item index="/backstage/member">
+            <i class="el-icon-user-solid"></i>
+            <span slot="title">会员管理</span>
+          </el-menu-item>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-s-platform"></i>
+              <span>广告管理</span>
+            </template>
+            <el-menu-item index="">广告列表</el-menu-item>
+            <el-menu-item index="">添加广告</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-s-goods"></i>
+              <span>产品管理</span>
+            </template>
+            <el-menu-item index="">产品列表</el-menu-item>
+            <el-menu-item index="">添加产品</el-menu-item>
+            <el-menu-item index="">产品类别</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="">
+            <i class="el-icon-s-home"></i>
+            <span slot="title">品牌介绍</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
       <el-container class="main-box">
-        <el-header>Header</el-header>
+        <el-header>
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              wolegejun<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-header>
         <el-main>
           <router-view />
         </el-main>
@@ -18,6 +64,14 @@ export default {
   data() {
     return {};
   },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
 };
 </script>
 
@@ -25,7 +79,7 @@ export default {
 .el-aside {
   -webkit-transition: width 0.28s;
   transition: width 0.28s;
-  width: 200px;
+  width: 200px !important;
   background-color: #304156;
   height: 100%;
   position: fixed;
@@ -37,7 +91,7 @@ export default {
   overflow: hidden;
 }
 .el-header {
-  height: 50px;
+  height: 50px !important;
   overflow: hidden;
   background-color: #fff;
   -webkit-box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
@@ -53,5 +107,15 @@ export default {
   transition: margin-left 0.28s;
   margin-left: 200px;
   position: relative;
+}
+
+.el-dropdown {
+  float: right;
+  margin-top: 15px;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  font-size: 16px;
+  color: #000000;
 }
 </style>
--- src/components/backstage/member-list.vue
+++ src/components/backstage/member-list.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    123456
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-input v-model="keywords" placeholder="请输入关键词"></el-input>
+      </el-col>
+      <el-col :span="6">
+        <el-button type="primary">查询</el-button>
+      </el-col>
+    </el-row>
+    <el-table :data="tableData" border style="width: 100%" class="mt20">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
+    <div class="mt20">
+      <el-button icon="el-icon-plus">创建会员</el-button>
+      <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+    </div>
   </div>
 </template>
 
@@ -8,11 +24,38 @@
 export default {
   name: "memberList",
   data() {
-    return {};
+    return {
+      keywords: "",
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style scoped>
-
+.el-pagination {
+  float: right;
+  margin-top: 4px;
+}
 </style>
